2348f83f97a57903d2464dc1b99d930bef8ec222
Josh Burgess <jburgess@whiteoctober.co.uk>
2017-10-30 09:40:08 +0000

    Add the start of our service worker

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 (function () {
+  if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('../service-worker.js')
+      .catch(console.error);
+  }
 } ());
---
+++ service-worker.js
@@ -0,0 +1,47 @@
+(function() {
+  'use strict';
+
+  var CACHE_NAME = 'AWDA_CACHE';
+  var urlsToCache = [
+    '.',
+    'index.html',
+    'css/main.css',
+    'js/main.js'
+  ];
+
+  self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+    );
+  });
+
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+      .then(function(response) {
+        return response || fetchAndCache(event.request);
+      })
+    );
+  });
+
+  function fetchAndCache(url) {
+    return fetch(url)
+      .then(function(response) {
+        // Check if we received a valid response
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+
+        return caches.open(CACHE_NAME)
+        .then(function(cache) {
+          cache.put(url, response.clone());
+          return response;
+        });
+      })
+      .catch(console.error);
+  }
+
+})();
