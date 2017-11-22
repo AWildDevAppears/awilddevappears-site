e03db2aa8b135ace910fa907370edbfc7f0eec22
Josh Burgess <jburgess@whiteoctober.co.uk>
2017-11-22 09:35:13 +0000

    Clean up the compile script

--- _compile.js
+++ _compile.js
@@ -1,6 +1,13 @@
 const pug = require('pug');
 const fs = require('fs');
 
+const PAGES = [
+  'index',
+  'about',
+  'projects',
+  'blog',
+  'styleguide'
+]
 
 const PROJECTS = [];
 
@@ -8,77 +15,41 @@ const POSTS = [
   'post-one'
 ]
 
-
 console.log('-- Building site --\n-- Transpiling pug files --\n');
 
-fs.writeFile('./index.html', pug.compileFile('_src/templates/index.pug', {
-  pretty: true
-})(), (err) => {
-   if(err) {
-        return console.error(err);
-    }
-    console.log('- Homepage created');
-});
-
-fs.writeFile('./about.html', pug.compileFile('_src/templates/about.pug', {
-  pretty: true
-})(), (err) => {
-   if(err) {
-        return console.error(err);
-    }
-    console.log('- About page created');
-});
-
-fs.writeFile('./projects.html', pug.compileFile('_src/templates/projects.pug', {
-  pretty: true
-})(), (err) => {
-   if(err) {
-        return console.error(err);
-    }
-    console.log('- Project page created');
-});
-
-fs.writeFile('./blog.html', pug.compileFile('_src/templates/blog.pug', {
-  pretty: true
-})(), (err) => {
-   if(err) {
-        return console.error(err);
-    }
-    console.log('- Blog listing created');
+PAGES.forEach((page) => {
+  fs.writeFile(`./${page}.html`, pug.compileFile(`_src/templates/${page}.pug`, {
+    pretty: true
+  })(), (err) => {
+     if(err) {
+          return console.error(err);
+      }
+      console.log(`- Page "${page}" created`);
+  });
 });
 
 
-fs.writeFile('./styleguide.html', pug.compileFile('_src/templates/styleguide.pug', {
-  pretty: true
-})(), (err) => {
-   if(err) {
-        return console.error(err);
+PROJECTS.forEach((project) => {
+  fs.writeFile(`./projects/${project}.html`, pug.compileFile(`_src/templates/projects/${project}.pug`, {
+    pretty: true
+  })(), (err) => {
+    if(err) {
+      return console.error(err);
     }
-    console.log('- Styleguide created');
+    console.log(`- Project page "${project}" created`);
+  });
 });
 
-PROJECTS.forEach( (project) => {
-  fs.writeFile('./projects/' + project + '.html', pug.compileFile('_src/templates/projects/' + project + '.pug', {
+POSTS.forEach((post) => {
+  fs.writeFile(`./blog/${post}.html`, pug.compileFile(`_src/templates/blog/${post}.pug`, {
       pretty: true
     })(), (err) => {
       if(err) {
             return console.error(err);
         }
-        console.log('- Project ' + project + ' created');
+        console.log(`- Blog post "${post}" created`);
   });
 });
-
-POSTS.forEach( (post) => {
-  fs.writeFile('./blog/' + post + '.html', pug.compileFile('_src/templates/blog/' + post + '.pug', {
-      pretty: true
-    })(), (err) => {
-      if(err) {
-            return console.error(err);
-        }
-        console.log('- BLOG ' + post + ' created');
-  });
-});
-
 
 
 
