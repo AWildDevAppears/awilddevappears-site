3898d4c00fe6bd4481ce2b5072144075c6e80a70
Josh Burgess <jburgess@whiteoctober.co.uk>
2017-11-22 10:18:19 +0000

    Begin plugging in an expandable nav

--- js/main.js
+++ js/main.js
@@ -3,4 +3,18 @@
     navigator.serviceWorker.register('../service-worker.js')
       .catch(console.error);
   }
+
+  document
+    .querySelector('[data-toggle]')
+    .addEventListener('click', function (e) {
+      var targetId = e.srcElement.getAttribute('data-toggle');
+      var targetElement = document.getElementById(targetId);
+
+      if (targetElement.classList.contains('expanded')) {
+        targetElement.classList.remove('expanded');
+        return;
+      }
+
+      targetElement.classList.add('expanded');
+    });
 } ());
