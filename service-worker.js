b24da6f7a4905579c57adee84737f296e20f2b32
Josh Burgess <jburgess@whiteoctober.co.uk>
2018-01-15 16:06:30 +0000

    Switch over to font awesome

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   var CACHE_NAME = 'AWDA_CACHE';
@@ -7,11 +7,28 @@
     'index.html',
     'css/main.min.css',
     'js/main.js',
-    'https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css',
+    'https://use.fontawesome.com/c3a9dd4412.js',
     'https://fonts.googleapis.com/css?family=Libre+Franklin:300|VT323',
   ];
 
-  self.addEventListener('install', function(event) {
+  function fetchAndCache(url) {
+    return fetch(url)
+      .then(function (response) {
+        // Check if we received a valid response
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+
+        return caches.open(CACHE_NAME)
+          .then(function (cache) {
+            cache.put(url, response.clone());
+            return response;
+          });
+      })
+      .catch(console.error);
+  }
+
+  self.addEventListener('install', function (event) {
     event.waitUntil(
       caches.open(CACHE_NAME)
         .then(function (cache) {
@@ -21,30 +38,12 @@
     );
   });
 
-  self.addEventListener('fetch', function(event) {
+  self.addEventListener('fetch', function (event) {
     event.respondWith(
       caches.match(event.request)
-        .then(function(response) {
+        .then(function (response) {
           return response || fetchAndCache(event.request);
         })
     );
   });
-
-  function fetchAndCache(url) {
-    return fetch(url)
-      .then(function(response) {
-        // Check if we received a valid response
-        if (!response.ok) {
-          throw Error(response.statusText);
-        }
-
-        return caches.open(CACHE_NAME)
-        .then(function (cache) {
-          cache.put(url, response.clone());
-          return response;
-        });
-      })
-      .catch(console.error);
-  }
-
-})();
+}());
